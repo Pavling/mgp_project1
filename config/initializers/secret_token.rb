ab950b63c204d6646c69610c429a13e1d85eecb5
Michael Pavling <pavling@gmail.com>
2014-02-03 09:15:56 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MgpProject1::Application.config.secret_token = 'cac91bacd251e165e97ccf1dda30d7a01f7b6d38dc2f66ba3ae8439593df08e51e980df738ae3e6ab956bf20a5b7b21cd0b6f1901d338cc0958e21b92ae2fbf5'
